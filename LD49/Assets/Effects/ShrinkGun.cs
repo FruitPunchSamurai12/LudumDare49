3ab38b4599e07802248253ceaf7069cf397378ce
FruitPunchSamurai12 <fruitpunchsamuraigr@gmail.com>
2021-10-03 03:21:31 -0700

    shrinking

--- LD49/Assets/Effects/ShrinkGun.cs
+++ LD49/Assets/Effects/ShrinkGun.cs
@@ -5,32 +5,65 @@ using UnityEngine;
 public class ShrinkGun : MonoBehaviour
 {
     Camera _cam;
-    [SerializeField] GameObject rayObj;
-    [SerializeField] float depth = -100;
-    bool firing = false;
+    [SerializeField] GameObject _rayObj;
+    LineRenderer _lineRenderer;
+    [SerializeField] float _depth = -100;
+    [SerializeField] float _range = 5f;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] float _shrinkRate;
+    [SerializeField] Material _shrinkMat;
+    bool _firing = false;
+    Shrinkable _currentShrinkable = null;
+
 
     private void Awake()
     {
         _cam = Camera.main;
+        _lineRenderer = _rayObj.GetComponent<LineRenderer>();
     }
 
     private void Update()
     {
         if(PlayerInput.Instance.Fire)
         {
-            Vector3 target = _cam.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, -depth));
-            rayObj.transform.LookAt(target);
-            rayObj.SetActive(true);
-            firing = true;
+            _rayObj.SetActive(true);
+            _firing = true;
         }
-        if(firing)
+        if(_firing)
         {
-            Vector3 target = _cam.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, -depth));
-            rayObj.transform.LookAt(target);
+            Vector3 target = _cam.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, -_depth));
+            Vector3 direction = (target - _rayObj.transform.position).normalized;
+            if(Physics.Raycast(_rayObj.transform.position,direction,out RaycastHit info, _range,_layerMask))
+            {
+                _lineRenderer.SetPosition(0, Vector3.zero);
+                _lineRenderer.SetPosition(1, _rayObj.transform.InverseTransformPoint(info.point));
+                var shrinkable = info.collider.GetComponent<Shrinkable>();
+                if(shrinkable!=null)
+                {
+                    if(_currentShrinkable!=null && _currentShrinkable !=shrinkable)
+                    {
+                        _currentShrinkable.StopShrink();
+                    }
+                    _currentShrinkable = shrinkable;
+                    shrinkable.Shrink(_shrinkMat, _shrinkRate);
+                }
+                Debug.Log(info.collider.name);
+            }
+            else
+            {
+                _lineRenderer.SetPosition(0, Vector3.zero);
+                _lineRenderer.SetPosition(1, _rayObj.transform.InverseTransformPoint(_rayObj.transform.position +direction *_range));
+            }
+            
             if (PlayerInput.Instance.StopFire)
             {
-                firing = false;
-                rayObj.SetActive(false);
+                if(_currentShrinkable!=null)
+                {
+                    _currentShrinkable.StopShrink();
+                    _currentShrinkable = null;
+                }
+                _firing = false;
+                _rayObj.SetActive(false);
             }
         }
     }
---
+++ LD49/Assets/Effects/Shrinkable.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrinkable : MonoBehaviour
+{
+    // Copy of the object with the ghost shader on it!
+    public GameObject _expandGraphics;
+    [SerializeField] Transform _parentPivot;
+    [SerializeField] float _revertingThreshold = 2f;
+    [SerializeField] float _revertingDuration = 2f;
+    [SerializeField] Vector3 _minimumScale = new Vector3(.3f, .3f, .3f);
+    bool _reverting = false;
+    float _revertingTime = 0;
+    MeshRenderer _rend;
+    Material _defaultMat;
+    Vector3 _startRevertingScale;
+
+    private void Awake()
+    {
+        _expandGraphics.SetActive(false);
+        _rend = GetComponent<MeshRenderer>();
+        _defaultMat = _rend.material;
+    }
+
+    void Update()
+    {
+        if(_reverting)
+        {
+            _revertingTime += Time.deltaTime;
+            if(_revertingTime>=_revertingThreshold)
+            {
+                float percentage = (_revertingTime - _revertingThreshold) / _revertingDuration;
+                _parentPivot.localScale = Vector3.Lerp(_minimumScale, Vector3.one, percentage);
+                if(_revertingTime>=(_revertingThreshold+_revertingDuration))
+                {
+                    _reverting = false;
+                    _rend.material = _defaultMat;
+                }
+            }
+        }
+    }
+
+    public void Shrink(Material shrinkMat,float shrinkRate)
+    {
+        if (_reverting)
+             _reverting = false;
+        _expandGraphics.SetActive(true);
+        _rend.material = shrinkMat;
+        _parentPivot.transform.localScale = Vector3.MoveTowards(_parentPivot.transform.localScale, _minimumScale, shrinkRate);
+    }
+
+    public void StopShrink()
+    {
+        _reverting = true;
+        _revertingTime = 0;
+    }
+}
